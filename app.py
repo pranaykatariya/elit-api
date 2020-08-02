8b9113a55f799811293d7cb146d11b5aeb2c8805
pranaykatariya <pranaykatariya1@gmail.com>
2020-08-02 23:42:24 +0530

    database field updated

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Created on Sun Mar  3 15:58:47 2019
 @author: sneha 
 """
   
-from flask import Flask 
+from flask import Flask  
 from flask_restful import reqparse,Api,Resource 
 from flask_cors import CORS, cross_origin
 import pandas as pd
